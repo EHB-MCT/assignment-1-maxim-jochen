c0130f34f0f3247cf56bdde6435bbea0e664d94e
JochenSunaert <Jochen.sunaert@student.ehb.be>
2024-10-17 17:19:24 +0200

    feat(pages): Add of a template in About.jsx and Home.jsx

---
+++ react-conventions/src/pages/About.jsx
@@ -0,0 +1,11 @@
+// About.jsx: The about page of the app.
+
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>Welcome to the About Page</h1>
+  </div>
+);
+
+export default About;
---
+++ react-conventions/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// Home.jsx: The main landing page of the app.
+
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Home Page</h1>
+  </div>
+);
+
+export default Home;
